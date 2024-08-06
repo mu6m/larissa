9dcaa23fe987d1c13940fc480f53fe898aee5ef7
Ledraa <121174019+mu6m@users.noreply.github.com>
2024-08-06 07:19:51 +0300

    Update larissa.go

--- larissa.go
+++ larissa.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type RSS struct {
+	Channel Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Items       []Item `xml:"item"`
+}
+
+type Item struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+}
 
 func main() {
-	fmt.Println("Larissa is coming ;)")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./rss_reader <rss_feed_url>")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching RSS feed:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		os.Exit(1)
+	}
+
+	var rss RSS
+	err = xml.Unmarshal(body, &rss)
+	if err != nil {
+		fmt.Println("Error parsing XML:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Feed: %s\n", rss.Channel.Title)
+	fmt.Printf("Description: %s\n\n", rss.Channel.Description)
+
+	for _, item := range rss.Channel.Items {
+		fmt.Printf("Title: %s\n", item.Title)
+		fmt.Printf("Link: %s\n", item.Link)
+		fmt.Printf("Description: %s\n\n", item.Description)
+	}
 }
